b7cfd7c24c6c1fbb4e1af4a7e53dfac53d2edaad
Diogo <2110319@my.ipleiria.pt>
2014-12-22 19:57:52 +0000

    Login + GetUtilizadores

---
+++ ClienteAndroid/src/com/example/acupunturaclienteandroid/webutils/RestClientException.java
@@ -0,0 +1,8 @@
+package com.example.acupunturaclienteandroid.webutils;
+
+public class RestClientException extends Exception {
+
+	public RestClientException(String detailMessage) {
+		super(detailMessage);
+	}
+}
